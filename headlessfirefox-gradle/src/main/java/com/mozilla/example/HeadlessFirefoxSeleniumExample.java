993bc94aa082ae81e7d31cdba4bfb71ca8553b71
phebous <phebous@users.noreply.github.com>
2018-07-25 15:45:02 -0700

    Update HeadlessFirefoxSeleniumExample.java

--- headlessfirefox-gradle/src/main/java/com/mozilla/example/HeadlessFirefoxSeleniumExample.java
+++ headlessfirefox-gradle/src/main/java/com/mozilla/example/HeadlessFirefoxSeleniumExample.java
@@ -12,7 +12,7 @@ public class HeadlessFirefoxSeleniumExample {
   public static void main(String [] args) {
     FirefoxBinary firefoxBinary = new FirefoxBinary();
     firefoxBinary.addCommandLineOptions("--headless");
-    System.setProperty("webdriver.gecko.driver", "/home/ndipiazza/Desktop/geckodriver");
+    System.setProperty("webdriver.gecko.driver", "/opt/firefox/geckodriver");
     FirefoxOptions firefoxOptions = new FirefoxOptions();
     firefoxOptions.setBinary(firefoxBinary);
     FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
